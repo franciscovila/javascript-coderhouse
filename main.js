48ff524c494033bfdb4484e049c81742f93bb5fe
Francisco <franvila50@gmail.com>
2022-07-04 14:00:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const listadoNotas = [5, 7, 3]
+
+
+let sumadorDeNotas = 0;
+
+
+for (let i =0; i < 3; i++){
+    sumadorDeNotas = sumadorDeNotas + listadoNotas[posicion];
+} 
+console.log (sumadorDeNotas);
