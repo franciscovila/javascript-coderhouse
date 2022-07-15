1d943accd5f48e84ab5330ced2b74af9911375ae
Francisco <franvila50@gmail.com>
2022-07-15 16:46:12 -0300

    arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,14 @@
-let totalCarrito = 0;
-let stockRemera = 5;
-let stockPantalon = 3;
-let stockBuzo = 2;
+const carrito = [];
 
-function agregarAlCarrito (producto, stock, ID) {
-        const tenemosStock = validarStock (stock);
-        if (tenemosStock) {
-            console.log("Agregar producto al carrito: " + producto);
-            totalCarrito++;
-            switch (ID) {
-                case 1:
-                    {
-                        stockRemera--
-                        break;
-                    }
-                case 2:
-                    {
-                        stockPantalon--
-                        break;
-                    }
-                case 3:
-                    {
-                        stockBuzo--
-                        break;
-                    }
-                }
-        }else{
-                console.log("Lo siento no tenemos stock");
-        } 
+function agregarAlCarrito(producto){
+carrito.push(producto);
+console.log(carrito);
 }
 
-function validarStock(stock) {
-    if (stock > 0) {
-            return true;
-        }else{
-            return false;
-        } 
-    }
 
 
-
+function borrarProductoDelCarrito(){
+        carrito.pop();
+        console.log(carrito);
+}
 
