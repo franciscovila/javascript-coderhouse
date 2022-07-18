89d90cc1fca9094cba2a72aabd627a51b7424998
Francisco <franvila50@gmail.com>
2022-07-18 12:00:21 -0300

    acumuladorYMaps

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,23 @@ function borrarProductoDelCarrito(){
         console.log(carrito);
 }
 
+// ACUMULADOR
+
+const miCompra = [
+{nombre: 'Remera negra XL', precio: 4200},
+{nombre: 'Pantalon Cargo', precio: 4000},
+{nombre: 'Buzo XL', precio: 5300}
+];
+const total = miCompra.reduce((acc, el) => acc + el.precio, 0);
+console.log(total); 
+
+// MAP 
+
+const actualizado = miCompra.map ((el) => {
+        return {
+                nombre: el.nombre,
+                precio: el.precio * 1.25 
+        }
+                });
+
+console.log(actualizado);
