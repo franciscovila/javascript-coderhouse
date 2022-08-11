8d762015a8f68a7aabe6bdd9191ade90711bdfc0
Francisco <franvila50@gmail.com>
2022-08-11 13:00:22 -0300

    &&

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,81 @@
-const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
-const total = carrito.reduce((acumulador, producto) => acumulador + producto.price, 0);
-document.getElementById('cartNumber').innerHTML = `${carrito.length} - $${total}`;
+const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
+const total = carrito.reduce(
+  (acumulador, producto) => acumulador + producto.price,
+  0
+);
+document.getElementById(
+  "cartNumber"
+).innerHTML = `${carrito.length} - $${total}`;
 
-
-
-
-function agregarAlCarrito(producto){
+function agregarAlCarrito(producto) {
 carrito.push(producto);
-console.log(carrito);
-let cartNumber = parseInt(document.getElementById('cartNumber').innerHTML);
-cartNumber++;
-document.getElementById('cartNumber').innerHTML = cartNumber;
-localStorage.setItem('carrito', JSON.stringify(carrito));
-const total = carrito.reduce((acumulador, producto) => acumulador + producto.price, 0);
-document.getElementById('cartNumber').innerHTML = `${carrito.length} - $${total}`;
+  console.log(carrito);
+  let cartNumber = parseInt(document.getElementById("cartNumber").innerHTML);
+  cartNumber++;
+  document.getElementById("cartNumber").innerHTML = cartNumber;
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  const total = carrito.reduce(
+    (acumulador, producto) => acumulador + producto.price,
+    0
+  );
+  document.getElementById(
+    "cartNumber"
+  ).innerHTML = `${carrito.length} - $${total}`;
 }
 
 
-function borrarProductoDelCarrito(){
-        carrito.pop(); 
-        console.log(carrito);
-        let cartNumber = parseInt(document.getElementById('cartNumber').innerHTML);
-        if (cartNumber != 0){
-        cartNumber--;
-        document.getElementById('cartNumber').innerHTML = cartNumber;  
-        localStorage.setItem('carrito', JSON.stringify(carrito));
-        const total = carrito.reduce((acumulador, producto) => acumulador + producto.price, 0);
-        document.getElementById('cartNumber').innerHTML = `${carrito.length} - $${total}`;
-        }
-        
-        
+
+function borrarProductoDelCarrito() {
+  carrito.pop();
+  console.log(carrito);
+  let cartNumber = parseInt(document.getElementById("cartNumber").innerHTML);
+  (cartNumber != 0) && cartRemover();
+  }
+
+function cartRemover() 
+ {
+  cartNumber--;
+  document.getElementById("cartNumber").innerHTML = cartNumber;
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  const total = carrito.reduce(
+    (acumulador, producto) => acumulador + producto.price,
+    0
+  );
+  document.getElementById(
+    "cartNumber"
+  ).innerHTML = `${carrito.length} - $${total}`;
 }
+
+
 
 const productos = [
-        {id: 1, title: 'Remera Negra XL', price: 5300, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/144/208/products/whatsapp-image-2021-03-27-at-16-42-00-31-1f5e74dddd15eeca4c16182787449474-240-0.jpeg"},
+  {
+    id: 1,
+    title: "Remera Negra XL",
+    price: 5300,
+    img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/144/208/products/whatsapp-image-2021-03-27-at-16-42-00-31-1f5e74dddd15eeca4c16182787449474-240-0.jpeg",
+  },
 
-        {id: 2, title: 'Pantalon Cargo', price: 4200, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/015/914/products/bclub11-e12dc76e00b566682016179053634854-1024-1024.jpg"},
+  {
+    id: 2,
+    title: "Pantalon Cargo",
+    price: 4200,
+    img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/015/914/products/bclub11-e12dc76e00b566682016179053634854-1024-1024.jpg",
+  },
 
-        {id: 3, title: 'Buzo XL', price: 4000, img: "https://cdn.shopify.com/s/files/1/0527/0008/6469/products/HOMBREB-NG-A_bece5452-3a7b-467c-841a-cf3e5d50e1ea_2048x.jpg?v=1647277362"}
-]
+  {
+    id: 3,
+    title: "Buzo XL",
+    price: 4000,
+    img: "https://cdn.shopify.com/s/files/1/0527/0008/6469/products/HOMBREB-NG-A_bece5452-3a7b-467c-841a-cf3e5d50e1ea_2048x.jpg?v=1647277362",
+  },
+];
 
-
-
-function prueba ()
-{
-for (const producto of productos) {
-        let contenedor = document.createElement("div");
-        contenedor.id = producto.id;
-        contenedor.innerHTML = `
+function prueba() {
+  for (const producto of productos) {
+    let contenedor = document.createElement("div");
+    contenedor.id = producto.id;
+    contenedor.innerHTML = `
 <div class="col mb-5">
 <div class="card h-100">
         <!-- Sale badge-->
@@ -71,20 +99,18 @@ href="#">Agregar al carrito</a></div>
 </div>
 </div>
 </div>
-`
-document.getElementById('cardContainer').appendChild(contenedor)
-}
+`;
+    document.getElementById("cardContainer").appendChild(contenedor);
+  }
 
-let botonsillo = document.createElement("div");
-botonsillo.innerHTML = `<div class="text-center"><a class="btn btn-primary mt-auto " 
+  let botonsillo = document.createElement("div");
+  botonsillo.innerHTML = `<div class="text-center"><a class="btn btn-primary mt-auto " 
 onclick= "borrarProductoDelCarrito(); "
 href="#">Borrar último producto del carrito</a>
-</div>`
-document.getElementById('cardContainer').appendChild(botonsillo)
+</div>`;
+  document.getElementById("cardContainer").appendChild(botonsillo);
 }
 
-prueba ();
+prueba();
 
-let botton = getElementById
-
-
+let botton = getElementById;
